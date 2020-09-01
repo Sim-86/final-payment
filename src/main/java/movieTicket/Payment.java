9c9114c717b676b3ac0167f7fe2e823f301c680d
P055900 <skt.p055900@partner.sk.com>
2020-09-01 14:03:14 +0900

    first commit

---
+++ src/main/java/movieTicket/Payment.java
@@ -0,0 +1,77 @@
+package movieTicket;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Payment_table")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long paymentId;
+    private Long bookingId;
+    private Double totalPrice;
+    private String paymentStatus;
+    private String paymentType;
+
+    @PostPersist
+    public void onPostPersist(){
+        PaymentSucceeded paymentSucceeded = new PaymentSucceeded();
+        BeanUtils.copyProperties(this, paymentSucceeded);
+        paymentSucceeded.publishAfterCommit();
+
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        PaymentCanceled paymentCanceled = new PaymentCanceled();
+        BeanUtils.copyProperties(this, paymentCanceled);
+        paymentCanceled.publishAfterCommit();
+
+
+    }
+
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+
+
+
+}
